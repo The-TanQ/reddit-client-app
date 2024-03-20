9a1b7e3f4d49fd0135d08e857eb1c50eca21f0dd
The-TanQ <tisetsoramolobeng@gmail.com>
2024-03-20 21:42:31 +0200

    Added main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './Features/Header/Header';
+import Home from './Features/Home/Home';
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Home />
+      </main>
     </>
   );
 }
---
+++ src/Features/Comment/Comment.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import moment from 'moment';
+import ReactMarkdown from 'react-markdown';
+import './Comment.css';
+
+const Comment = (props) => {
+  const { comment } = props;
+  return (
+    <div className="comment">
+      <div className="comment-metadata">
+        <p className="comment-author">{comment.author}</p>
+        <p className="comment-created-time">
+          {moment.unix(comment.created_utc).fromNow()}
+        </p>
+      </div>
+      <ReactMarkdown children={comment.body} />
+    </div>
+  );
+};
+
+export default Comment;
