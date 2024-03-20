5eb31ae906776188ab9b15ad49f26a5f178ff48d
The-TanQ <tisetsoramolobeng@gmail.com>
2024-03-20 20:59:55 +0200

    Added Header and utils

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import Header from './Features/Header/Header';
 
 function App() {
   return (
     <>
-      
+      <Header />
     </>
   );
 }
